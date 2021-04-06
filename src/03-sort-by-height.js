cb346f5b2c3328a033787b99655a62931ceb6d9c
Lenar <flfforwork@gmail.com>
2021-04-07 00:23:23 +0300

    feat: add solve sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,23 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const arrCopy = arr.slice();
+  let iterator = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      arrCopy.splice(iterator, 1);
+    } else {
+      iterator += 1;
+    }
+  }
+  arrCopy.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      arrCopy.splice(i, 0, -1);
+    }
+  }
+  return arrCopy;
 }
 
 module.exports = sortByHeight;
