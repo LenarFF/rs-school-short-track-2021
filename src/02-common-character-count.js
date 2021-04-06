64fbdfd3696550bb9834dacd2f590db3a4166045
Lenar <flfforwork@gmail.com>
2021-04-06 23:45:30 +0300

    feat: add common-character-count solve

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,18 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const firstString = s1.split('');
+  let res = 0;
+  let search = null;
+  for (let i = 0; i < s2.length; i++) {
+    search = firstString.indexOf(s2[i]);
+    if (search !== -1) {
+      res += 1;
+      firstString.splice(search, 1);
+    }
+  }
+  return res;
 }
 
 module.exports = getCommonCharacterCount;
