107412529d1ee9ff38bbed49c70300be1e7b1055
LenarFF <flfforwork@gmail.com>
2021-04-08 13:20:27 +0300

    feat: add solve find-index

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,12 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  const x = array.length;
+  for (let i = x - 1; i > -1; i--) {
+    if (array[i] === value) return i;
+  }
+  return false;
 }
 
 module.exports = findIndex;
