919c26647b0b979d493e6522813a0372c23c986c
LenarFF <flfforwork@gmail.com>
2021-04-08 13:39:46 +0300

    feat: add solve encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const obj = {};
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in obj && str[i] === str[i - 1]) {
+      obj[str[i]] += 1;
+    } else {
+      obj[str[i]] = 1;
+      arr.push(str[i]);
+    }
+  }
+  let result = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]] !== 1) {
+      result += `${obj[arr[i]]}${arr[i]}`;
+    } else {
+      result += `${arr[i]}`;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
