4f896f9d8e8873fb9e633fa4b987e9d1324889a7
LenarFF <flfforwork@gmail.com>
2021-04-08 14:05:55 +0300

    feat: add solve dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,25 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const arr = [];
+  const obj = {};
+  let dns = '';
+  for (let i = 0; i < domains.length; i++) {
+    arr.push(domains[i].split('.'));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    dns = '';
+    for (let j = arr[i].length - 1; j > -1; j--) {
+      dns += `.${arr[i][j]}`;
+      if (dns in obj) {
+        obj[dns] += 1;
+      } else {
+        obj[dns] = 1;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
